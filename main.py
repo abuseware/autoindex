574146619c7ab77ec0ec162b285280d72a913af8
Artur Kaleta <49493036+Abuseware@users.noreply.github.com>
2013-12-02 18:38:49 +0100

    PEP8

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
     License:    MIT
 """
 
-import sys
 import os
 import flask
 
@@ -14,26 +13,27 @@ import utils
 
 web = flask.Flask(__name__)
 
+
 @web.route('/')
 def index():
     return flask.redirect('/browse')
+
 
 @web.route('/browse')
 @web.route('/browse/')
 @web.route('/browse/<path:path>')
 def browse(path=''):
-
-    fullpath = flask.request.environ['DOCUMENT_ROOT']+os.sep+path
+    fullpath = flask.request.environ['DOCUMENT_ROOT'] + os.sep + path
 
     folders = []
     files = []
-   
+
     path = utils.fill_path(path)
 
     if os.path.isdir(fullpath):
         try:
             for entry in os.listdir(fullpath):
-                if os.path.isfile(fullpath+os.sep+entry):
+                if os.path.isfile(fullpath + os.sep + entry):
                     files.append(entry)
                 else:
                     folders.append(entry)
@@ -42,26 +42,25 @@ def browse(path=''):
             folders.sort()
             files.sort()
             return flask.render_template('browse.html', path=path, nav=nav, folders=folders, files=files)
-    
+
     return flask.abort(404)
 
 
 @web.route('/info')
 @web.route('/info/<path:path>')
 def info(path=''):
+    fullpath = flask.request.environ['DOCUMENT_ROOT'] + os.sep + path
+    properties = []
 
-    fullpath = flask.request.environ['DOCUMENT_ROOT']+os.sep+path
-    
     if os.path.isfile(fullpath):
         try:
-            nav = utils.populate_navbar(path)
             properties = [
-                ['File', path],                                             
+                ['File', path],
                 ['Size', utils.get_size(fullpath)],
                 ['Mimetype', utils.get_mime(fullpath)],
             ]
         finally:
+            nav = utils.populate_navbar(path)
             return flask.render_template('info.html', path=path, nav=nav, properties=properties)
 
     return flask.abort(404)
-
--- utils.py
+++ utils.py
@@ -5,29 +5,32 @@ import mimetypes
 
 mimetypes.init()
 
+
 def fill_path(path):
     if path[:1] != '/':
-        path = '/'+path
+        path = '/' + path
     if path[-1:] != '/':
         path += '/'
 
     return path
 
+
 def get_size(path):
-    power_map = ['B','KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+    power_map = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
 
     siz = os.path.getsize(path)
     power = 0
-    
+
     while siz > 1024:
         siz /= 1024
         power += 1
-    
-    return '{} {}'.format(round(siz,2),power_map[power])
+
+    return '{} {}'.format(round(siz, 2), power_map[power])
+
 
 def get_mime(path):
     mime = mimetypes.guess_type(path)[0]
-    if mime == None:
+    if not mime:
         mime = "application/octet-stream"
 
     return mime
@@ -47,7 +50,7 @@ def populate_navbar(path):
             if index == 0:
                 entries.append([navlist[index], navlist[index]])
             else:
-                entries.append([entries[index-1][0] + os.sep + navlist[index], navlist[index]])
+                entries.append([entries[index - 1][0] + os.sep + navlist[index], navlist[index]])
 
     return entries
 
